290b7f4bdb3f42fdfc2e40d8b77871c77858ff6f
MD-Lai <marvin.dhlai@gmail.com>
2019-08-27 14:37:02 +1000

    ngram works in theory
    takes ~0.05 secs to classify one tweet.
    ~5 hours to classify all tests.

--- code/models/baseline/most_frequent_handle.py
+++ code/models/baseline/most_frequent_handle.py
@@ -8,6 +8,6 @@ def train(data):
 class MostFrequentModel:
     def __init__(self, handles):
         handles = np.array([int(handle) for handle in handles])
-        self.mostFreq = str(np.bincount(self.handles).argmax())
+        self.mostFreq = str(np.bincount(handles).argmax())
     def predict(self, tweet):
         return self.mostFreq
--- code/models/baseline/simple_ngram.py
+++ code/models/baseline/simple_ngram.py
@@ -1,27 +1,70 @@
 import numpy as np
 from collections import defaultdict
+import time
 
-def train(data):
-    model = SimpleNGramModel(data)
+def train(data):    
+
+    ngramLen = 3
+    L = 200
+
+    model = SimpleNGramModel(ngramLen)
+    
+    for t in data:
+        model.feed(t.handle, t.text)
+    
+    model.trim(L)
+
     return model
 
+def _ddictpickle(): # needed to pickle the module
+    return defaultdict(int)
+
 class SimpleNGramModel:
-    def __init__(self, data):
-        self.handles = [t.handle for t in data]
-        self.text = [t.text for t in data]
+    def __init__(self, ngramLen):
 
-        self.ngrams = defaultdict(defaultdict(int)) # {handle: {ngram: count, ...}, ...}
+        self.ngramLen = ngramLen
 
-        for t in data:
+        self.ngrams = defaultdict(_ddictpickle) # {handle: {ngram: count, ...}, ...}
+        # After triming converted to {handle: set(top_L_ngrams)}
 
-            self.ngrams[t.handle][t.]
 
-    # don't include start and end characters (for simplicity)
-    def generateNgrams(self, text, n):
-        # deal with n > len(text)?
+    def feed(self, handle, text):
+        for ngs in self.generateNgrams(text):
+            self.ngrams[handle][ngs] += 1
+
+    # keep L most frequent ngrams (including count?)
+    def trim(self, L):
+        for hg in self.ngrams.items():
+            handle = hg[0]
+            grams = hg[1]
+
+            topL = sorted(grams.items(), key = lambda x: x[1], reverse = True)[:L]
+
+            # If not keeping count: 
+            # dict((n, c) for n,c in topL) -> set([n for n,_ in topL])
+            self.ngrams[handle] = set([n for n,_ in topL])  
+
+    def predict(self, tweet):
+        
+        tweetGrams = self.generateNgrams(tweet)
+        tweetGramsSet = set(tweetGrams)
+        highestMatch = 0
+        highestHandle = "?????"
+
+        for hn in self.ngrams.items():
+            matchCount = len(tweetGramsSet.intersection(hn[1]))
+
+            if matchCount > highestMatch:
+                highestMatch = matchCount
+                highestHandle = hn[0]
+
+        return highestHandle
+
+    def generateNgrams(self, text):
+        n = self.ngramLen
         ngrams = []
-        for i in range(len(text)-n):
-            ngram = ""
-            for j in range(n):
-                pass
+        textPadded = ("_"*(n-1)) + text + ("_"*(n-1))
+        for i in range(len(textPadded)-n+1):
+            ngrams.append(textPadded[i:i+n])
                 
+        return ngrams
