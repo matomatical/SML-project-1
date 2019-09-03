022df4923e13aa46f92f238f6646329d8c6ccbfb
Matt Farrugia <m@far.in.net>
2019-09-03 16:12:36 +1000

    rearrange tune script to make it easier to parallelise

--- code/tune.py
+++ code/tune.py
@@ -8,21 +8,14 @@ from tqdm import tqdm
 import sklearn.model_selection as model_selection
 import numpy as np
 
-import data; data.load_train()
+import data
 import eval
 
-# which model to experiment with?
-MODULE = "models.baseline.simple_ngram"
-MODEL  = importlib.import_module(MODULE).Model
 
-# what values of hyperparameters define the grid?
-# keys: hyper parameter names, values: list of values for hyper parameters
-GRID = model_selection.ParameterGrid({
-    "L": [200, 500, 1000],
-    "n": [2, 3, 4, 5]
-})
+# import the specified model:
 
 # generate and fold training data
+data.load_train()
 DATA = np.array(data.TRAIN)
 N_SPLITS = 8
 RANDOM = 51
@@ -31,30 +24,58 @@ FOLDS = list(model_selection.KFold(n_splits=N_SPLITS, random_state=RANDOM).split
 # where do results go?
 LOGFILEDIR  = "experiments"
 os.makedirs(LOGFILEDIR, exist_ok=True)
-LOGFILENAME = os.path.join(LOGFILEDIR, MODULE + ".jsonl") # log results in JSON-line format
 
 
-# Let the science begin! #uwu
-bar_grid = tqdm(GRID, desc="Parameter combinations", position=2, dynamic_ncols=True)
-for params in bar_grid:
-    tqdm.write(f"Parameter combination: {params}")
-    
-    # begin experiment for this parameter combination:
-    accuracies = []
-    bar_folds = tqdm(FOLDS, desc="Cross-validation", position=1, leave=False, dynamic_ncols=True)
-    for i, (train_ids, test_ids) in enumerate(bar_folds, start=1):
-        # train and evalute the model on this data split:
-        bar_train = tqdm(DATA[train_ids], desc="Training...", position=0, leave=False, dynamic_ncols=True)
-        model = MODEL(bar_train, **params)
-        bar_eval = tqdm(DATA[test_ids], desc="Evaluating...", position=0, leave=False, dynamic_ncols=True)
-        accuracy, *_ = eval.evaluate(model, bar_eval)
-        accuracies.append(accuracy)
-        tqdm.write(f"Test {i} of {params} complete. Fold accuracy: {accuracy:.2%}")
-        # and don't forget to record the results to the log file!
-        with open(LOGFILENAME, 'a') as logfile:
-            results = {'params': params, 'fold': i, 'accuracy': accuracy, 'time': time.time()}
-            print(json.dumps(results), file=logfile, flush=True)
+def main():    
+    # Define the experiments here: ------------------------------------------ #
+    # which model to experiment with?
+    MODULE = "models.baseline.simple_ngram"
 
-    # experiment done! compute the average result
-    avg_accuracy = sum(accuracies)/len(accuracies)
-    tqdm.write(f"Finished testing {params}. Mean accuracy from {N_SPLITS} folds: {avg_accuracy:.2%}")
+    # what values of hyperparameters define the grid?
+    # keys: hyper parameter names, values: list of values for hyper parameters
+    GRID = model_selection.ParameterGrid({
+        "L": [200, 500, 1000],
+        "n": [2, 3, 4, 5]
+    })
+
+    # Where to save the results?
+    LOGFILENAME = os.path.join(LOGFILEDIR, MODULE + ".jsonl")
+    # (default: experiments/MODULE_NAME.jsonl)
+    # ----------------------------------------------------------------------- #
+
+    # Let the science begin! #uwu
+    model_class = importlib.import_module(MODULE).Model
+    bar_grid = tqdm(GRID, desc="Parameter combinations", position=2, dynamic_ncols=True)
+    for params in bar_grid:
+        tqdm.write(f"Parameter combination: {params}")
+        
+        # begin experiments for this parameter combination:
+        accuracies = []
+        bar_folds = tqdm(FOLDS, desc="Cross-validation", position=1, leave=False, dynamic_ncols=True)
+        for i, fold in enumerate(bar_folds, start=1):
+            accuracy = experiment(params, (i, fold), model_class, LOGFILENAME)
+            tqdm.write(f"Test {i} of {params} complete. Fold accuracy: {accuracy:.2%}")
+            accuracies.append(accuracy)
+        # experiments done! compute the average result over folds
+        avg_accuracy = sum(accuracies)/len(accuracies)
+        tqdm.write(f"Finished testing {params}. Mean accuracy from {N_SPLITS} folds: {avg_accuracy:.2%}")
+
+def experiment(params, fold, model_class, logfilename):
+    # unpack the data split for this experiment
+    fold_id, (train_ids, test_ids) = fold
+
+    # train and evalute the model on this data split:
+    bar_train = tqdm(DATA[train_ids], desc="Training...", position=0, leave=False, dynamic_ncols=True)
+    model = model_class(bar_train, **params)
+    bar_eval = tqdm(DATA[test_ids], desc="Evaluating...", position=0, leave=False, dynamic_ncols=True)
+    accuracy, *_ = eval.evaluate(model, bar_eval)
+
+    # don't forget to record the results to the log file!
+    with open(logfilename, 'a') as logfile:
+        results = {'params': params, 'fold': fold_id, 'accuracy': accuracy, 'time': time.time()}
+        print(json.dumps(results), file=logfile, flush=True)
+
+    return accuracy
+
+if __name__ == '__main__':
+    main()
