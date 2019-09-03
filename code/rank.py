c5f306447f9d10cff60b5a9d6519f402f3a25996
Matt Farrugia <m@far.in.net>
2019-09-04 06:43:05 +1000

    overnight experiments and improve rank script

--- code/rank.py
+++ code/rank.py
@@ -1,4 +1,5 @@
-import sys
+import os
+import glob
 import json
 from collections import defaultdict
 from statistics import mean
@@ -32,37 +33,41 @@ FOOTER = """</main>\n</body>\n</html>\n"""
 
 
 def main():
-    print(HEADER)
-    for filename in sys.argv[1:]:
-        print(TABLE_HEADER.format(filename=filename))
+    with open("experiments/experiments-summary.html", 'w') as file:
+        print(HEADER, file=file)
+        for filename in glob.glob("experiments/*.jsonl"):
+            print("processing", filename)
+            print(TABLE_HEADER.format(filename=filename), file=file)
+            
+            # load the results
+            results = []
+            with open(filename) as infile:
+                results = [json.loads(line) for line in infile]
+
+            # collate and rank
+            collated_results = defaultdict(list)
+            for result in results:
+                params = tuple(result['params'].items())
+                collated_results[params].append((result['fold'], result['accuracy']))
+            scored_results = []
+            for params, fold_accs in collated_results.items():
+                folds, accs = zip(*fold_accs)
+                scored_result = {
+                    'params': ', '.join('='.join(map(str, p)) for p in params),
+                    'accuracy': mean(accs), # TODO: Include variance?
+                    'folds': ', '.join(map(str, folds))
+                }
+                scored_results.append(scored_result)
+            ranked_results = sorted(scored_results, key=lambda r: -r['accuracy'])
+
+            # generate table row
+            for rank, result in enumerate(ranked_results, start=1):
+                print(TABLE_ROW.format(rank=rank, **result), file=file)
+            print(TABLE_FOOTER, file=file)
         
-        # load the results
-        results = []
-        with open(filename) as file:
-            results = [json.loads(line) for line in file]
-
-        # collate and rank
-        collated_results = defaultdict(list)
-        for result in results:
-            params = tuple(result['params'].items())
-            collated_results[params].append((result['fold'], result['accuracy']))
-        scored_results = []
-        for params, fold_accs in collated_results.items():
-            folds, accs = zip(*fold_accs)
-            scored_result = {
-                'params': ', '.join('='.join(map(str, p)) for p in params),
-                'accuracy': mean(accs), # TODO: Include variance?
-                'folds': ', '.join(map(str, folds))
-            }
-            scored_results.append(scored_result)
-        ranked_results = sorted(scored_results, key=lambda r: -r['accuracy'])
-
-        # generate table row
-        for rank, result in enumerate(ranked_results, start=1):
-            print(TABLE_ROW.format(rank=rank, **result))
-        print(TABLE_FOOTER)
-    
-    print(FOOTER)
+        print(FOOTER, file=file)
+    filename = os.path.abspath("experiments/experiments-summary.html")
+    print(f"saved result in file://{filename}")
 
 
 if __name__ == '__main__':
