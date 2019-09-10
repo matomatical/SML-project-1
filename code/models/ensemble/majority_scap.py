610eaf61a0dab05efe77510b5118e9be7bd8f10c
Matt Farrugia <m@far.in.net>
2019-09-10 14:23:41 +1000

    add flexible patterns to ensemble

--- code/models/ensemble/majority_scap.py
+++ code/models/ensemble/majority_scap.py
@@ -1,22 +1,26 @@
 from collections import Counter
 
+from tqdm import tqdm
+
 from models.baseline.simple_ngram import Model as BaseModel
 
-PARAM_GRID = [{'n': 2, 'level': "byte", 'norm': "False", 'L': "200"},
-              {'n': 3, 'level': "char", 'norm': "False", 'L': "300"},
-              {'n': 4, 'level': "char", 'norm': "False", 'L': "400"},
-              {'n': 5, 'level': "char", 'norm': "False", 'L': "400"},
-              {'n': 6, 'level': "char", 'norm': "False", 'L': "400"},
-              {'n': 2, 'level': "word", 'norm': "True",  'L': "200"}]
+PARAM_GRID = [
+    {'n': 2, 'level': "byte", 'norm': "False", 'L': "200"},
+    {'n': 3, 'level': "char", 'norm': "False", 'L': "300"},
+    {'n': 4, 'level': "char", 'norm': "False", 'L': "400"},
+    {'n': 5, 'level': "char", 'norm': "False", 'L': "400"},
+    {'n': 6, 'level': "char", 'norm': "False", 'L': "400"},
+    {'n': 2, 'level': "word", 'norm': "True",  'L': "200"},
+    {'n': 2, 'level': "flex", 'norm': "True",  'L': "150"}
+]
                 
 
 class Model:
     def __init__(self, data):
         data = list(data) # strip iterators, sorry tqdm!
-        self.models = [BaseModel(data, **params) for params in PARAM_GRID]
+        self.ntweets = Counter(t.handle for t in tqdm(data))
+        self.models = [BaseModel(tqdm(data), **params) for params in tqdm(PARAM_GRID)]
     def predict(self, tweet):
-        return majority(model.predict(tweet) for model in self.models)
-
-def majority(items):
-    return Counter(items).most_common(1)[0][0]
-        
+        votes = Counter(model.predict(tweet) for model in self.models)
+        majority = max(votes.keys(), key=lambda a: (votes[a], self.ntweets[a]))
+        return majority
